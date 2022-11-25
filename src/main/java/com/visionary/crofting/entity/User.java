1f2f4b7a0b661340d39c91eaea560f0ca54c4ce6
omar kazoum <omarKazoum96@gmail.com>
2022-11-25 12:20:19 +0100

    added sample entity

---
+++ src/main/java/com/visionary/crofting/entity/User.java
@@ -0,0 +1,20 @@
+package com.visionary.crofting.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+    String name;
+}
