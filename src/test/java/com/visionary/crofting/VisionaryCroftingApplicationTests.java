819befe67bfae9cfe4ba25f6607ba27513ac902c
omar kazoum <omarKazoum96@gmail.com>
2022-11-25 11:07:21 +0100

    intial project

---
+++ src/test/java/com/visionary/crofting/VisionaryCroftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionary.crofting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionaryCroftingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
