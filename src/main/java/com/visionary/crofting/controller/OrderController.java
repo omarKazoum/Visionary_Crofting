f1f7ba71fce142f51e5c732a9ce6364896405f81
omar kazoum <omarKazoum96@gmail.com>
2022-11-26 23:33:36 +0100

    configured connection with postgres database

--- src/main/java/com/visionary/crofting/controller/OrderController.java
+++ src/main/java/com/visionary/crofting/controller/OrderController.java
@@ -1,7 +1,20 @@
 package com.visionary.crofting.controller;
 
+import com.visionary.crofting.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController("/api/orders")
 public class OrderController {
+    @GetMapping("/test")
+    ApiResponse<String> test(){
+        ApiResponse response=new ApiResponse<>();
+        response.setResponseCode(ApiResponse.ResponseCode.SUCCESS);
+
+        response.setData("great work");
+        response.setResponseMessage("everything went well");
+        return response;
+    }
 }
---
+++ src/main/java/com/visionary/crofting/response/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.visionary.crofting.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private ResponseCode responseCode;
+    private String responseMessage;
+    private T data;
+
+    public enum ResponseCode{
+        SUCCESS,
+        VALIDATION_ERROR,
+        NOT_ALLOWED,
+        INVALID_CREDENTIALS,
+        INVALID_TOKEN
+    }
+}
