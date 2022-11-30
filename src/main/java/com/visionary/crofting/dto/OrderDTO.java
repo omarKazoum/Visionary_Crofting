c18f42f547209a533e40f26f0f27190c30515286
omar kazoum <omarKazoum96@gmail.com>
2022-11-30 09:47:45 +0100

    fixed a bug in beans

---
+++ src/main/java/com/visionary/crofting/dto/OrderDTO.java
@@ -0,0 +1,20 @@
+package com.visionary.crofting.dto;
+
+import com.visionary.crofting.entity.Order;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class OrderDTO {
+    private Long id;
+    private Date date;
+    private String reference;
+    private Date createdAt = new Date();
+    private double totalPrice;
+    private Long clientId;
+    private Order.OrderStatusEnum status;
+
+}
