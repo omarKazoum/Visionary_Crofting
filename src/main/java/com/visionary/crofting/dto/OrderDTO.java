2e168fcd11aca6646f6b697a7ea1a8b21c8c6ab6
Omar kazoum <72218455+omarKazoum@users.noreply.github.com>
2022-11-30 16:45:57 +0100

    added orders endpoints (#15)
    * implemented orderItem update and delete
    created required classes for orderItem and order endpoints
    
    * finished order items endpoints
    
    * implemented get order items per order endpoints
    
    * fixed a bug in beans
    
    * fixed problem in imports after moving DTO classes to a specific package
    
    * added Order endpoints

--- src/main/java/com/visionary/crofting/dto/OrderDTO.java
+++ src/main/java/com/visionary/crofting/dto/OrderDTO.java
@@ -1,5 +1,6 @@
 package com.visionary.crofting.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.visionary.crofting.entity.Order;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,14 @@ import java.util.Date;
 @NoArgsConstructor
 public class OrderDTO {
     private Long id;
+    @JsonFormat
+            (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private Date date;
     private String reference;
+    @JsonFormat
+            (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private Date createdAt = new Date();
-    private double totalPrice;
+    private double totalPrice=-1;
     private Long clientId;
     private Order.OrderStatusEnum status;
-
 }
